7fb4e0f4b3f2c26e95e6feb59a80fa2488aba26e
YangXiang <1215627787@qq.com>
2022-12-06 17:36:30 +0800

    feat: 新增业务管理文件

---
+++ src/components/CommonTable.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+type TableHeaderType = {
+  prop: string,
+  label: string,
+  width: '200px'
+}
+type TableConditionType = {
+  page: number,
+  size: number,
+  total: number,
+  seoForm: Array<{ [prop: string]: any }>
+}
+const props = defineProps<{
+  tableData: Array<unknown>,
+  tableHeader: TableHeaderType[],
+  tableCondition: TableConditionType
+  add?: string
+}>();
+
+const { tableData, tableHeader, tableCondition, add } = $$(props);
+const copyTableCondition: TableConditionType = JSON.parse(JSON.stringify(tableCondition));
+
+const handleReset = () => {
+  for (const key in tableCondition) {
+    (tableCondition as any)[key] = copyTableCondition[key as keyof TableConditionType];
+  }
+}
+</script>
+
+<template>
+  <div class="common-table">
+    <div class="search">
+      <div class="search-left">
+        <template
+          v-for="item in tableCondition.seoForm"
+        >
+          <el-input
+            v-if="item.type === 'input'"
+            v-model="item.value"
+            :placeholder="item.placeholder"
+            :clearable="item.clearable === undefined ? true : item.clearable"
+          ></el-input>
+          <el-select
+            v-if="item.type === 'select'"
+            v-model="item.value"
+            :placeholder="item.placeholder"
+            :clearable="item.clearable === undefined ? true : item.clearable"
+          >
+            <el-option
+              v-for="opt in item.option"
+              :label="opt.label"
+              :value="opt.value"
+            ></el-option>
+          </el-select>
+        </template>
+      </div>
+      <div class="search-right">
+        <el-button @click="handleReset">重置</el-button>
+        <el-button type="primary" @add="$emit('search')">搜索</el-button>
+        <el-button v-if="add" type="success" @add="$emit('add')">{{ add || '添加' }}</el-button>
+      </div>
+    </div>
+    <el-table :data="tableData" border>
+      <el-table-column
+        v-for="item in tableHeader"
+        :prop="item.prop"
+        :label="item.label"
+        :minWidth="item.width"
+      ></el-table-column>
+    </el-table>
+    <div class="page">
+      <el-pagination
+        background
+        layout="prev, pager, next, sizes"
+        :total="100"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.common-table {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.search {
+  flex-shrink: 0;
+  display: flex;
+  flex-wrap: wrap;
+  &-right {
+    display: flex;
+    justify-content: flex-end;
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+  .el-input, .el-select {
+    width: 250px;
+    margin-right: 15px;
+    margin-bottom: 10px;
+  }
+}
+.el-table {
+  flex: 1;
+  margin: 15px 0;
+}
+.page {
+  display: flex;
+  height: 32px;
+  flex-shrink: 0;
+  justify-content: flex-end;
+}
+</style>
---
+++ src/pages/business/BusinessMessage.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const tableHeader = $ref([
+  { prop: 'name', label: '名称', width: '200px' },
+  { prop: 'title', label: '标题', width: '200px' },
+  { prop: 'content', label: '内容', width: '200px' },
+]);
+const tableData = $ref([
+  { name: 'name1', title: 'title1', content: 'content1' },
+  { name: 'name2', title: 'title2', content: 'content2' },
+  { name: 'name3', title: 'title3', content: 'content3' },
+]);
+const tableCondition = $ref({
+  page: 1,
+  size: 10,
+  total: 112,
+  seoForm: {
+    name: {
+      type: 'input',
+      value: '',
+      placeholder: '请输入名称搜索'
+    },
+    title: {
+      type: 'select',
+      value: '1',
+      placeholder: '请选择标题类型',
+      option: [
+        { label: '值1', value: '1' },
+        { label: '值2', value: '2' }
+      ]
+    },
+    name2: {
+      type: 'input',
+      value: '',
+      placeholder: '请输入名称搜索'
+    },
+  }
+})
+</script>
+
+<template>
+  <CommonTable :tableData="tableData" :tableHeader="tableHeader" :tableCondition="tableCondition"></CommonTable>
+</template>
+
+<style lang="scss">
+
+</style>
