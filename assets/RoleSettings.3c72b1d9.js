493dc6da8ca07815945fa8001f9c3d9f68c1065b
Aizener <1215627787@qq.com>
2023-01-31 22:43:13 +0800

    feat: preview

---
+++ assets/RoleSettings.3c72b1d9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.93aa0e70.js";const t={};function n(r,c){return" RoleSettings "}const s=e(t,[["render",n]]);export{s as default};
---
+++ assets/nesting1-1.9d735713.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as n,c as s,I as _,f as c}from"./index.93aa0e70.js";const r={},a={class:"nesting"};function d(i,p){const e=o("router-view");return n(),s("div",a,[_(" \u5D4C\u5957\u8DEF\u75311-1 "),c(e)])}const f=t(r,[["render",d],["__scopeId","data-v-789b6b84"]]);export{f as default};
